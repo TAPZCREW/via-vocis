acf9b8920a8786ce869f7bd42e263eccd2cd4f52
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-09-11 19:45:21 +0200

    add some policies for dev

--- xmake.lua
+++ xmake.lua
@@ -1,3 +1,6 @@
+set_policy("build.c++.modules.two_phases", false)
+set_policy("build.c++.modules.non_cascading_changes", true)
+
 includes("driver/xmake.lua")
 includes("qt-frontend/xmake.lua")
 includes("cli-frontend/xmake.lua")
